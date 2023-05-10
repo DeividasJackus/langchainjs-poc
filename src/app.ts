eec4a1e7482bd31f665c34c0dced9713231370f7
Deividas Jackus <DeividasJackus@users.noreply.github.com>
2023-05-11 00:11:23 +0200

    add summarization example

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as process from "node:process";
 import * as util from "node:util";
+import * as fs from "node:fs";
 import * as dotenv from "dotenv";
 import {
 	PromptTemplate,
@@ -8,14 +9,15 @@ import {
 	ChatPromptTemplate,
 	MessagesPlaceholder,
 } from "langchain/prompts";
-import { ConversationChain, LLMChain } from "langchain/chains";
+import { ConversationChain, LLMChain, loadSummarizationChain, AnalyzeDocumentChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
-import { OpenAI } from "langchain/llms/openai";
+import { OpenAI, OpenAI } from "langchain/llms/openai";
 import { AgentExecutor, ChatAgent, initializeAgentExecutorWithOptions } from "langchain/agents";
 import { SerpAPI } from "langchain/tools";
 import { Calculator } from "langchain/tools/calculator";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "langchain/schema";
+import { LangChainHub } from "langchain-hub";
 
 dotenv.config();
 
@@ -154,22 +156,22 @@ const { HELICONE_API_KEY = "", OPENAI_API_KEY = "", SERPAPI_API_KEY = "" } = pro
 // console.log(util.inspect(response, { depth: null }));
 
 // 12
-const chat = new ChatOpenAI(
-	{ temperature: 0 },
-	{
-		basePath: "https://oai.hconeai.com/v1",
-		baseOptions: {
-			headers: {
-				"Helicone-Auth": `Bearer ${HELICONE_API_KEY}`,
-			},
-		},
-	},
-);
-const tools = [new SerpAPI(SERPAPI_API_KEY)];
-const agent = ChatAgent.fromLLMAndTools(chat, tools);
-const executor = AgentExecutor.fromAgentAndTools({ agent, tools });
-const response = await executor.run("What's the weather in Copenhagen, celsius?");
-console.log(util.inspect(response, { depth: null }));
+// const chat = new ChatOpenAI(
+// 	{ temperature: 0 },
+// 	{
+// 		basePath: "https://oai.hconeai.com/v1",
+// 		baseOptions: {
+// 			headers: {
+// 				"Helicone-Auth": `Bearer ${HELICONE_API_KEY}`,
+// 			},
+// 		},
+// 	},
+// );
+// const tools = [new SerpAPI(SERPAPI_API_KEY)];
+// const agent = ChatAgent.fromLLMAndTools(chat, tools);
+// const executor = AgentExecutor.fromAgentAndTools({ agent, tools });
+// const response = await executor.run("What's the weather in Copenhagen, celsius?");
+// console.log(util.inspect(response, { depth: null }));
 
 // 13
 // const chat = new ChatOpenAI({ temperature: 0 });
@@ -207,3 +209,28 @@ console.log(util.inspect(response, { depth: null }));
 // 	],
 // });
 // await chat.call([new HumanChatMessage("Write me a song about sparkling water.")]);
+
+// 15
+// CAUTION: Token heavy!
+// In this example, we use the `AnalyzeDocumentChain` to summarize a large text document.
+// const text = fs.readFileSync("state_of_the_union.txt", "utf8");
+// const model = new OpenAI(
+// 	{ temperature: 0 },
+// 	{
+// 		basePath: "https://oai.hconeai.com/v1",
+// 		baseOptions: {
+// 			headers: {
+// 				"Helicone-Auth": `Bearer ${HELICONE_API_KEY}`,
+// 			},
+// 		},
+// 	},
+// );
+// const combineDocsChain = loadSummarizationChain(model);
+// const chain = new AnalyzeDocumentChain({
+// 	combineDocumentsChain: combineDocsChain,
+// });
+// const result = await chain.call({
+// 	// eslint-disable-next-line @typescript-eslint/naming-convention
+// 	input_document: text,
+// });
+// console.log({ result });
